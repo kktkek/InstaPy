726979a7a5879285ff231552c64fc22a77f58b99
kktkek <kktkekekekke@gmail.com>
2025-08-05 05:48:42 +0300

    Update test_file_handling.py
    سحب

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,11 +1,21 @@
 import unittest
 
 from instapy import util
-
-
+https://www.instagram.com/xee_0_
+?igsh=c3p1Z2c3Z3Q1MHcz
+- [Smileys & Emotion](#smileys--emotion)
+- [People & Body](#people--body)
+- [Animals & Nature](#animals--nature)
+- [Food & Drink](#food--drink)
+- [Travel & Places](#travel--places)
+- [Activities](#activities)
+- [Objects](#objects)
+- [Symbols](#symbols)
+- [Flags](#flags)
+- [GitHub Custom Emoji](#github-custom-emoji)
 class TestFileHandling(unittest.TestCase):
     """Unit tests for file_handling function"""
-
+1.1../@.com+
     # list of expected entries
     expected_entries = ["This is a comment...", "photography", "portrait"]
 
